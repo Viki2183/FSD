23f783357f66fa1362fa88d82e35b28f01f36c59
Viki2183 <vrishinkp21@gmail.com>
2023-07-11 10:55:24 +0530

    praticals

---
+++ practical 1/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Componenets/Home/Home';
+import Contact from './Componenets/Contact us/Contact';
+import About from './Componenets/About us/About';
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="Wrapper">
+      <h1>Marine Mammals</h1>
+      <BrowserRouter>
+       <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/about" element={<About/>}/>
+        <Route exact path="/Contact" element={<Contact/>}/>
+       </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
